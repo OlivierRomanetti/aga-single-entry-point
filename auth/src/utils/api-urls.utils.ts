e841517cd9e299d1c0f9fa64486005da9bcad54b
Olivier Romanetti <olivier.romanetti@gmail.com>
2023-03-08 16:04:14 -0500

    apiUrls & fix bug

--- auth/src/utils/api-urls.utils.ts
+++ auth/src/utils/api-urls.utils.ts
@@ -6,6 +6,13 @@ export const apiUrls = [
     apiKey: '7PQF8Pp3uuxcNjE7L9YB3ZRkTRdCKhmER8poP4VLoctuKVu95Ccx9MBLRFXumGA5',
   },
   {
+    name: 'Adherents',
+    domain: 'https://adherents.aga.ca/',
+    url: 'https://intqa.api.adherents.mws-alithya.com/api/login-portal/users',
+    apiKey: '661588e7fa9a44dcb59ee0689243146d9732c58f6bc2e39b893999b99bbe9ab8',
+  },
+
+  {
     name: 'Assurances-Collectives',
     domain: 'https://assurancescollectives.com/',
     url: 'https://api.assurancescollectives.com/v1/users',
