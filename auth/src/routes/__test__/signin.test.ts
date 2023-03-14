d5c00bf174b2635ea1c85444933fe41217059d83
Olivier Romanetti <olivier.romanetti@gmail.com>
2023-03-14 15:27:14 -0400

    unit test for backend

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,50 +1,118 @@
 import request from 'supertest';
 import { app } from '../../app';
+import nock from 'nock';
+import { apiUrls } from '../../utils/api-urls.utils';
 
-it('fails when a email that does not exist is supplied', async () => {
+const username = 'username';
+
+it('fails when the username is empty', async () => {
   await request(app)
     .post('/api/users/signin')
     .send({
-      email: 'test@test.com',
-      password: 'password'
+      username: '',
     })
     .expect(400);
 });
 
-it('fails when an incorrect password is supplied', async () => {
-  await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201);
-
+it('fails if the username is not in any of the portals', async () => {
+  apiUrls.forEach((api) => {
+    nock(api.url)
+      .matchHeader('apiKey', api.apiKey)
+      .get('')
+      .query({ username })
+      .reply(200, { exist: false, loginUrl: '' });
+  });
   await request(app)
     .post('/api/users/signin')
     .send({
-      email: 'test@test.com',
-      password: 'aslkdfjalskdfj'
+      username,
     })
     .expect(400);
 });
 
-it('responds with a cookie when given valid credentials', async () => {
+it('fails if the username is in all portals', async () => {
+  apiUrls.forEach((api) => {
+    nock(api.url)
+      .matchHeader('apiKey', api.apiKey)
+      .get('')
+      .query({ username })
+      .reply(200, { exist: true, loginUrl: '/login' });
+  });
   await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201);
-
-  const response = await request(app)
     .post('/api/users/signin')
     .send({
-      email: 'test@test.com',
-      password: 'password'
+      username,
+    })
+    .expect(400);
+});
+
+it('fails if the username is in two portals', async () => {
+  const [api1, api2, ...otherApis] = apiUrls;
+  nock(api1.url)
+    .matchHeader('apiKey', api1.apiKey)
+    .get('')
+    .query({ username })
+    .reply(200, { exist: true, loginUrl: '/login' });
+
+  nock(api2.url)
+    .matchHeader('apiKey', api2.apiKey)
+    .get('')
+    .query({ username })
+    .reply(200, { exist: true, loginUrl: '/login' });
+
+  otherApis.forEach((api, index) => {
+    nock(api.url)
+      .matchHeader('apiKey', api.apiKey)
+      .get('')
+      .query({ username })
+      .reply(200, { exist: false, loginUrl: '' });
+  });
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      username,
+    })
+    .expect(400);
+});
+
+it('fails if one apiKey is not correct', async () => {
+  const [api1, ...otherApis] = apiUrls;
+  nock(api1.url).get('').query({ username }).reply(401);
+
+  otherApis.forEach((api) => {
+    nock(api.url)
+      .matchHeader('apiKey', api.apiKey)
+      .get('')
+      .query({ username })
+      .reply(200, { exist: false, loginUrl: '' });
+  });
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      username,
+    })
+    .expect(400);
+});
+
+it('succeed if the username is in one and only one portal', async () => {
+  const [api1, ...otherApis] = apiUrls;
+  nock(api1.url)
+    .matchHeader('apiKey', api1.apiKey)
+    .get('')
+    .query({ username })
+    .reply(200, { exist: true, loginUrl: '/login' });
+
+  otherApis.forEach((api) => {
+    nock(api.url)
+      .matchHeader('apiKey', api.apiKey)
+      .get('')
+      .query({ username })
+      .reply(200, { exist: false, loginUrl: '' });
+  });
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      username,
     })
     .expect(200);
-
-  expect(response.get('Set-Cookie')).toBeDefined();
 });
