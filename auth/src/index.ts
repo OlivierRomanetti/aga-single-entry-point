2f2b976be71f897c2085c9139a633aade90cecbd
Olivier Romanetti <olivier.romanetti@gmail.com>
2023-03-16 15:26:00 -0400

    add adhesion

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import { app } from './app';
 
 const start = async () => {
   app.listen(3000, () => {
+    // TODO: PORT à remplacer avec variable d'environnement
     console.log('Listening on port 3000!!!!!!!!');
   });
 };
--- auth/src/utils/api-urls.utils.ts
+++ auth/src/utils/api-urls.utils.ts
@@ -11,6 +11,12 @@ export const apiUrls = [
     url: 'https://intqa.api.adherents.mws-alithya.com/api/login-portal/users',
     apiKey: '661588e7fa9a44dcb59ee0689243146d9732c58f6bc2e39b893999b99bbe9ab8',
   },
+  {
+    name: 'Adhesion',
+    domain: 'https://test-adhesion.aga.ca/',
+    url: 'https://test-adhesion.aga.ca/api/users',
+    apiKey: 'bf4a906219934f7bf6858209ffcaf7c486a73fcb891f39baab398a9899adbba8',
+  },
 
   {
     name: 'Assurances-Collectives',
