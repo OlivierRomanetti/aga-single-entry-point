d0da5ec36361d39156de372c9ba63a2535550dac
Olivier Romanetti <olivier.romanetti@gmail.com>
2023-04-19 10:57:50 -0400

    update auth tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,4 +1,8 @@
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JDBENEFITS_API_KEY = 'JDBENEFITS_API_KEY';
+  process.env.ADHERENTS_API_KEY = 'ADHERENTS_API_KEY';
+  process.env.ADHESION_API_KEY = 'ADHESION_API_KEY';
+  process.env.WBLLIBERT_API_KEY = 'WBLLIBERT_API_KEY';
+  process.env.PPIBMW_API_KEY = 'PPIBMW_API_KEY';
+  process.env.OLIVERAPP_API_KEY = 'OLIVERAPP_API_KEY';
 });
